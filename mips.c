68ff3322d134bcfab9ad1d60b63c629eb679b200
enzobjm <enzobjm@usp.br>
2018-05-29 15:25:05 -0300

    Alu

--- mips.c
+++ mips.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <math.h>
 
-#define ZERO "000000000000000000000000000000000000"
 
 //Memory of 64 word (256 bytes) as global variable
 unsigned char MEM[64][32];
@@ -46,7 +45,7 @@ unsigned char ALUOp[2];
 unsigned char ALUSrcB[2];
 
 //All aux variables
-unsigned char PcIn = '0';
+unsigned char PcIn = 0;
 
 //32 Bit aux Instruction Register
 unsigned char AuxInstructionRegister[32];
@@ -69,6 +68,10 @@ unsigned char AuxREG[32][32];
 //aux ALUOUT
 unsigned char AuxAluOut[32];
 
+//ZERO AND ONE
+unsigned char Z[1] = {0};
+unsigned char O[1] = {1};
+
 
 //Control unit state
 int old=0;
@@ -77,7 +80,7 @@ int next=0;
 int convertCharToInt(unsigned char *binaryArray, int lenght){
     int result = 0;
     for (int i = (lenght-1); i >= 0; --i){
-        if(binaryArray[i] == '1')
+        if(binaryArray[i] == 1)
             result += pow(2, (lenght-1)-i);
     }
     return result;
@@ -144,9 +147,9 @@ MemtoReg[1]=0;
 return;
 }
 
-void PC_func(unsigned char mxPC [32]){
+void PC_func(unsigned char mxPC [32], unsigned char PcIn){
 	int i;
-	if(PcIn == '1'){
+	if(PcIn == 1){
 		for(i = 0; i < 32; i++){
 			PC[i] = mxPC[i];
 		}
@@ -158,13 +161,13 @@ void PC_func(unsigned char mxPC [32]){
 void memory(unsigned char adress[32], unsigned char data[32], unsigned char read, unsigned char write)
 {
 	//For a read operation
-	if(read == '1')	
+	if(read == 1)	
 	{
 		//Copy memory data to return_array
 		int adr = convertCharToInt(adress, 32);
 		for(int i = 0; i < 32; i++)
 		{
-			if(Irwrite == '1')
+			if(Irwrite == 1)
 			{
 				AuxInstructionRegister[i] = MEM[adr][i];
 			}
@@ -172,7 +175,7 @@ void memory(unsigned char adress[32], unsigned char data[32], unsigned char read
 		}
 	}
 	//For a write operation
-	else if(write == '1')
+	else if(write == 1)
 	{
 		//Copy memory data to return_array
 		int adr = convertCharToInt(adress, 32);
@@ -186,7 +189,7 @@ void memory(unsigned char adress[32], unsigned char data[32], unsigned char read
 void registers(unsigned char ReadRegister1[5], unsigned char ReadRegister2[5], unsigned char WriteRegister[5], unsigned char WriteData[32], unsigned char RegWrite)	
 {
 	//To write WriteData content at register of number WriteRegister
-	if(RegWrite == '1')
+	if(RegWrite == 1)
 	{
 		int reg_num = convertCharToInt(WriteRegister, 5);
 		for (int i = 0; i < 32; ++i)
@@ -211,13 +214,13 @@ unsigned char *mux2(
     unsigned char *input3,
     unsigned char *control
 ) {
-    if(control[0] == '0') {
-        if(control[1] == '0')
+    if(control[0] == 0) {
+        if(control[1] == 0)
             return input0;
         else
             return input2;
     } else {
-        if(control[1] == '0')
+        if(control[1] == 0)
             return input1;
         else
             return input3;
@@ -229,7 +232,7 @@ unsigned char *mux(
     unsigned char *input1,
     unsigned char control
 ) {
-    if(control == '0') {
+    if(control == 0) {
         return input0;
         
     } 
@@ -245,8 +248,8 @@ unsigned char *shiftLeft(unsigned char *input) {
     for (i = 31; i >= 0; i--) {
         result[i-2] = input[i];
     }
-    result[31] = '0';
-    result[30] = '0';
+    result[31] = 0;
+    result[30] = 0;
 
     return result;
 }
@@ -266,24 +269,24 @@ unsigned char *signalExtend(unsigned char *input) {
 }
 
 int checkFunctionField(unsigned char *instructionFunction){
-    if(instructionFunction[0] == '1' && instructionFunction[1] == '0' && instructionFunction[2] == '0' 
-        && instructionFunction[3] == '0' && instructionFunction[4] == '0' && instructionFunction[5] == '0'){
+    if(instructionFunction[0] == 1 && instructionFunction[1] == 0 && instructionFunction[2] == 0 
+        && instructionFunction[3] == 0 && instructionFunction[4] == 0 && instructionFunction[5] == 0){
 
         return 0;
-    } else if (instructionFunction[0] == '1' && instructionFunction[1] == '0' && instructionFunction[2] == '0' 
-        && instructionFunction[3] == '0' && instructionFunction[4] == '1' && instructionFunction[5] == '0'){
+    } else if (instructionFunction[0] == 1 && instructionFunction[1] == 0 && instructionFunction[2] == 0 
+        && instructionFunction[3] == 0 && instructionFunction[4] == 1 && instructionFunction[5] == 0){
        
         return 1;
-    } else if (instructionFunction[0] == '1' && instructionFunction[1] == '0' && instructionFunction[2] == '0' 
-        && instructionFunction[3] == '1' && instructionFunction[4] == '0' && instructionFunction[5] == '0'){
+    } else if (instructionFunction[0] == 1 && instructionFunction[1] == 0 && instructionFunction[2] == 0 
+        && instructionFunction[3] == 1 && instructionFunction[4] == 0 && instructionFunction[5] == 0){
         
         return 2;
-    } else if (instructionFunction[0] == '1' && instructionFunction[1] == '0' && instructionFunction[2] == '0' 
-        && instructionFunction[3] == '1' && instructionFunction[4] == '0' && instructionFunction[5] == '1'){
+    } else if (instructionFunction[0] == 1 && instructionFunction[1] == 0 && instructionFunction[2] == 0 
+        && instructionFunction[3] == 1 && instructionFunction[4] == 0 && instructionFunction[5] == 1){
 
         return 3;
-    } else if (instructionFunction[0] == '1' && instructionFunction[1] == '0' && instructionFunction[2] == '1' 
-        && instructionFunction[3] == '0' && instructionFunction[4] == '1' && instructionFunction[5] == '0'){
+    } else if (instructionFunction[0] == 1 && instructionFunction[1] == 0 && instructionFunction[2] == 1 
+        && instructionFunction[3] == 0 && instructionFunction[4] == 1 && instructionFunction[5] == 0){
 
         return 4;
     }
@@ -291,24 +294,24 @@ int checkFunctionField(unsigned char *instructionFunction){
 }
 
 int checkALUoperation(unsigned char *aluControlInput){
-    if(aluControlInput[0] == '0' && aluControlInput[1] == '0' && aluControlInput[2] == '1' 
-        && aluControlInput[3] == '0'){
+    if(aluControlInput[0] == 0 && aluControlInput[1] == 0 && aluControlInput[2] == 1 
+        && aluControlInput[3] == 0){
 
         return 0;
-    } else if (aluControlInput[0] == '0' && aluControlInput[1] == '1' && aluControlInput[2] == '1' 
-        && aluControlInput[3] == '0'){
+    } else if (aluControlInput[0] == 0 && aluControlInput[1] == 1 && aluControlInput[2] == 1 
+        && aluControlInput[3] == 0){
        
         return 1;
-    } else if (aluControlInput[0] == '0' && aluControlInput[1] == '0' && aluControlInput[2] == '0' 
-        && aluControlInput[3] == '0'){
+    } else if (aluControlInput[0] == 0 && aluControlInput[1] == 0 && aluControlInput[2] == 0 
+        && aluControlInput[3] == 0){
         
         return 2;
-    } else if (aluControlInput[0] == '0' && aluControlInput[1] == '0' && aluControlInput[2] == '0' 
-        && aluControlInput[3] == '1'){
+    } else if (aluControlInput[0] == 0 && aluControlInput[1] == 0 && aluControlInput[2] == 0
+        && aluControlInput[3] == 1){
 
         return 3;
-    } else if (aluControlInput[0] == '0' && aluControlInput[1] == '1' && aluControlInput[2] == '1' 
-        && aluControlInput[3] == '1'){
+    } else if (aluControlInput[0] == 0 && aluControlInput[1] == 1 && aluControlInput[2] == 1 
+        && aluControlInput[3] == 1){
 
         return 4;
     }
@@ -319,68 +322,68 @@ unsigned char *aluControl(unsigned char *ALUop, unsigned char *instructionFuncti
     unsigned char *result = malloc(sizeof(unsigned char) * 4);
 
     //sum Store/Load word
-    if(ALUop[0] == '0' && ALUop[1] == '0'){
-        result[0] = '0';
-        result[1] = '0';
-        result[2] = '1';
-        result[3] = '0';
+    if(ALUop[0] == 0 && ALUop[1] == 0){
+        result[0] = 0;
+        result[1] = 0;
+        result[2] = 1;
+        result[3] = 0;
         return result;
     }
     // branch subtraction
-    else if (ALUop[0] == '0' && ALUop[1] == '1'){
-        result[0] = '0';
-        result[1] = '1';
-        result[2] = '1';
-        result[3] = '0';
+    else if (ALUop[0] == 0 && ALUop[1] == 1){
+        result[0] = 0;
+        result[1] = 1;
+        result[2] = 1;
+        result[3] = 0;
         return result;
     }
-    else if (ALUop[0] == '1' && ALUop[1] == '1'){
-        result[0] = '0';
-        result[1] = '0';
-        result[2] = '0';
-        result[3] = '0';
+    else if (ALUop[0] == 1 && ALUop[1] == 1){
+        result[0] = 0;
+        result[1] = 0;
+        result[2] = 0;
+        result[3] = 0;
         return result;
     }
     // R-type
-    else if (ALUop[0] == '1' && ALUop[1] == '0'){
+    else if (ALUop[0] == 1 && ALUop[1] == 0){
         //Sum
         if (checkFunctionField(instructionFunction) == 0){
-            result[0] = '0';
-            result[1] = '0';
-            result[2] = '1';
-            result[3] = '0';
+            result[0] = 0;
+            result[1] = 0;
+            result[2] = 1;
+            result[3] = 0;
             return result;
         }
         //Subtraction
         else if (checkFunctionField(instructionFunction) == 1){
-            result[0] = '0';
-            result[1] = '1';
-            result[2] = '1';
-            result[3] = '0';
+            result[0] = 0;
+            result[1] = 1;
+            result[2] = 1;
+            result[3] = 0;
             return result;
         }
         //And
         else if (checkFunctionField(instructionFunction) == 2){
-            result[0] = '0';
-            result[1] = '0';
-            result[2] = '0';
-            result[3] = '0';
+            result[0] = 0;
+            result[1] = 0;
+            result[2] = 0;
+            result[3] = 0;
             return result;
         }
         // or
         else if (checkFunctionField(instructionFunction) == 3){
-            result[0] = '0';
-            result[1] = '0';
-            result[2] = '0';
-            result[3] = '1';
+            result[0] = 0;
+            result[1] = 0;
+            result[2] = 0;
+            result[3] = 1;
             return result;
         }
         //Set on less than
         else if (checkFunctionField(instructionFunction) == 4){
-            result[0] = '0';
-            result[1] = '1';
-            result[2] = '1';
-            result[3] = '1';
+            result[0] = 0;
+            result[1] = 1;
+            result[2] = 1;
+            result[3] = 1;
             return result;
         }
     }
@@ -391,7 +394,7 @@ unsigned char *decToBinary(int n){
     unsigned char *result = malloc(sizeof(unsigned char) * 32);
     // Size of an integer is assumed to be 32 bits
     for (int i = 31; i >= 0; i--) {
-        result[31 - i] = n & 1 << i ? '1' : '0';
+        result[31 - i] = n & 1 << i ? 1 : 0;
     }
     return result;
 }
@@ -484,16 +487,49 @@ unsigned char *split(int init, int end, unsigned char *vetor)
 	return array;
 }
 
+
+unsigned char Pc_In(unsigned char write, unsigned char cond, unsigned char *b_n_e)
+{
+	return (b_n_e&&cond)||write;
+}
+
 int main()
 {
-	//Init 
+	
+	unsigned char **op;
+
+	op = (unsigned char **)malloc(sizeof(unsigned char*) * 64);
 	for (int i = 0; i < 32; ++i)
 	{
-		InstructionRegister[i] = MEM[0][i];
+		op[i] = (unsigned char *)malloc(sizeof(unsigned char) * 32);
 	}
 
-	while(1)
+	for (int i = 0; i < 32; ++i)
 	{
+		op[0][i] = decToBinary(201326610)[i];
+		op[1][i] = decToBinary(537395210)[i];
+		op[2][i] = decToBinary(537460737)[i];
+		op[3][i] = decToBinary(537526274)[i];
+		op[4][i] = decToBinary(277348355)[i];
+		op[5][i] = decToBinary(8986656)[i];
+		op[6][i] = decToBinary(13185058)[i];
+		op[7][i] = decToBinary(134217732)[i];
+		op[8][i] = decToBinary(8929322)[i];
+		op[9][i] = decToBinary(549912577)[i];
+		op[10][i] = decToBinary(14702629)[i];
+	}
+
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 32; ++j)
+		{
+			MEM[i][j] = op[i][j];
+		}
+	}
+	
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("%d\n", convertCharToInt(AuxPC,32));
 		//Unit control 
 		unit_control(split(26,31,InstructionRegister));
 		//Memory block funcion
@@ -502,15 +538,29 @@ int main()
 		registers(split(21,25,InstructionRegister), split(16,20,InstructionRegister), mux2(split(16,20,InstructionRegister),
 				  split(11,15, InstructionRegister), decToBinary(31), NULL, RegDst), mux2(AluOut, MemoryDataRegister, PC, NULL, MemtoReg), RegWrite);
 		//Alu function
-		ALU(mux(A, PC, ALUSrcA), mux(B, decToBinary(4), signalExtend(split(0, 15, InstructionRegister), 
-			shiftLeft(signalExtend(split(0, 15, InstructionRegister))), ALUSrcB)), aluControl(ALUOp, split(0,5, InstructionRegister)));
+		ALU(mux(A, PC, ALUSrcA), mux2(B, decToBinary(1), signalExtend(split(0, 15, InstructionRegister)),shiftLeft(signalExtend(split(0, 15, InstructionRegister))), ALUSrcB), aluControl(ALUOp, split(0,5, InstructionRegister)));
 		//PC function
-		PC()		
-
-
-
-
+		PC_func(mux2(AuxAluOut, AluOut, shiftLeft(split(0,25,InstructionRegister)), A, PCSource), Pc_In(PCWrite, PCWriteCond, mux(Z,O,BNE)));
+		//Pass aux values to registers at upper clock border
 		clockUpper();
+	}
+	printf("REGISTRADORES\n");
+	for (int i = 0; i < 32; ++i)
+	{
+		for (int j = 0; j < 32; ++j)
+		{
+			printf("%hhu",REG[i][j]);
+		}
+		printf("\n");
+	}
+	printf("MEMORIA:\n");
+	for (int i = 0; i < 64; ++i)
+	{
+		for (int j = 0; j < 32; ++j)
+		{
+			printf("%hhu", MEM[i][j]);
+		}
+		printf("\n");
 	}
 
 	return 0;
